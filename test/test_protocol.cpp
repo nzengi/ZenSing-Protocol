822b465b0d01ba6f992d7f4f059c8861594b9d35
nzengi <zenginureddin1@gmail.com>
2024-08-20 05:32:38 +0300

    Create test_protocol.cpp

---
+++ test/test_protocol.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "protocol.h"
+
+int main() {
+    int L = 1024, N = 160;
+    Parameters params = choose_parameters(L, N);
+
+    Signer signer(params);
+    User user(params, signer.keypair.y);
+
+    std::vector<uint8_t> info = {'i', 'n', 'f', 'o'};
+    std::vector<uint8_t> msg = {'m', 'y', ' ', 'm', 's', 'g'};
+
+    signer.start(info);
+    user.start(info, msg);
+
+    auto [a, b] = signer.one();
+    uint64_t e = user.two(a, b);
+    auto [r, c, s, d] = signer.three(e);
+    auto [rho, omega, delta, sigma] = user.four(r, c, s, d);
+
+    std::cout << "Signer and User operations successful!" << std::endl;
+    return 0;
+}
